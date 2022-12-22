a1900c4f5ff118437e81826caef8659e59c4534d
georgevjr03 <georgevjr03@gmail.com>
2022-12-22 15:59:11 +0530

    Video player App

---
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.video_player
+
+import android.R
+import android.net.Uri
+import android.net.Uri.parse
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.video_player.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val videoView=findViewById<VideoView>(binding.testView.id)
+        val mediaController=MediaController(this)
+        mediaController.setAnchorView(videoView)
+        val uri: Uri=parse("android.resource://" + packageName+ "/raw/test1")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+
+
+    }
+
+}
